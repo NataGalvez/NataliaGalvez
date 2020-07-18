86cf3b9f81c45b7f35870726bb29024b399edcfa
Natalia Gálvez Caroca <nataliac.galvezc@gmail.com>
2020-07-18 15:18:16 -0400

    heroku ok

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + '/dist/nataliagalvez'))
+app.use(express.static(__dirname + '/dist/galveznata'))
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/nataliagalvez/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/galveznata/index.html'))
 })
 
 app.listen(process.env.PORT || 8080)
